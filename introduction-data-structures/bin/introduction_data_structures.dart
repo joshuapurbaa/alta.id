35d5d833cd4cecbbebd52cb30b20e8e01a73c751
joshuapurbaa <yoshuapurba06@gmail.com>
2022-08-26 20:12:17 +0700

    Membuat project dart baru introduction-data-structures

---
+++ introduction-data-structures/bin/introduction_data_structures.dart
@@ -0,0 +1,5 @@
+import 'package:introduction_data_structures/introduction_data_structures.dart' as introduction_data_structures;
+
+void main(List<String> arguments) {
+  print('Hello world: ${introduction_data_structures.calculate()}!');
+}
---
+++ introduction-data-structures/test/introduction_data_structures_test.dart
@@ -0,0 +1,8 @@
+import 'package:introduction_data_structures/introduction_data_structures.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
