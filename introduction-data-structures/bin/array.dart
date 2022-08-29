12785300eb07c41e1ef2dcd7851273c4ffe8ee7d
joshuapurbaa <yoshuapurba06@gmail.com>
2022-08-30 04:56:59 +0700

    --

--- introduction-data-structures/bin/array.dart
+++ introduction-data-structures/bin/array.dart
@@ -8,4 +8,16 @@ void main() {
   // Searching
   var indeksDurian = array.indexOf('Durian');
   print(indeksDurian); // console: 3
+
+  // Insertion
+  array.add('Frambos');
+  print(array); // console: [Anggur, Belimbing, Ceri, Durian, Erbis, Frambos]
+
+  // Insertion value di awal array
+  array.insert(0, 'Frambos');
+  print(array); // console: [Frambos, Anggur, Belimbing, Ceri, Durian, Erbis]
+
+  // Insertion value di tengah array
+  array.insert(2, 'Frambos');
+  print(array); // console: [Anggur, Belimbing, Frambos, Ceri, Durian, Erbis]
 }
