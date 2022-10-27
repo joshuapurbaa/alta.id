63f8c3e52a6210f5c91bd3a43761f134be942498
joshuapurbaa <yoshuapurba06@gmail.com>
2022-10-28 05:24:58 +0700

    Delet some code

--- debuggin_demo/lib/main.dart
+++ debuggin_demo/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 import 'screens/detail_screen.dart';
 import 'screens/home_screen.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
   runApp(const MyApp());
 }
 
